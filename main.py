5fc64b6ef4a30698f4a76469cdb01683b8384e8b
Harsh <harshg421994@gmail.com>
2023-12-10 13:19:16 +0000

    first_day_1

---
+++ main.py
@@ -0,0 +1,11 @@
+print("""Harsh
+December 10th 2023
+I am signing up for the Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so Ican do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling :D 
+You can follow my progress at replit.com/@harshg421994""")
