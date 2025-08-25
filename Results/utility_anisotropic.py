7a45945e2a11dc512a5b6edcb2f21a65352c79dd
Harsh Gupta <82764276+harsh-quantum@users.noreply.github.com>
2025-08-25 12:09:41 +0530

    newbranch

---
+++ Results/utility_anisotropic.py
@@ -0,0 +1,131 @@
+# Basic utility functions used by all simulation codes for Aniosotropic model..
+
+import random
+
+# Keep track of X and Z errors separately. 
+class Errors(object):
+  def __init__(self, x, z):
+    self.x = x
+    self.z = z
+
+class ErrorRates(object):
+  def __init__(self, prep, cnot, meas):
+    self.prep = prep
+    self.cnot = cnot 
+    self.meas = meas
+  
+# Generate random error(s) on specific qubit(s). 
+def errorsX1(bit, errors, rate): 
+  if random.random() < rate: 
+    errors.x ^= 1<<bit
+
+def errorsZ1(bit, errors, rate): 
+  if random.random() < rate: 
+    errors.z ^= 1<<bit
+
+def errors1(bit, errors, rate): 
+  r = random.random()
+  if r < rate: 
+    r = 1 + int(3 * r / rate)
+    if r&1: 
+      errors.x ^= 1<<bit
+    if r>>1&1: 
+      errors.z ^= 1<<bit
+
+def errors2(bit1, bit2, errors, rate): 
+  r = random.random()    
+  if r < rate: 
+    r = 1 + int(15 * r / rate)  
+    if r&1: 
+      errors.x ^= 1<<bit1
+    if r>>1&1:
+      errors.z ^= 1<<bit1
+    if r>>2&1:
+      errors.x ^= 1<<bit2
+    if r>>3&1:
+      errors.z ^= 1<<bit2
+
+# Propogation of error, without and with fault.
+def cnot0(bit1, bit2, errors): 
+  errors.x ^= (errors.x>>bit1&1)<<bit2
+  errors.z ^= (errors.z>>bit2&1)<<bit1
+
+def hadamard0(bit, errors): 
+  errxz = (errors.x & (1<<bit)) ^ (errors.z & (1<<bit))
+  errors.x ^= errxz
+  errors.z ^= errxz
+
+def cz0(bit1, bit2, errors): 
+  hadamard0(bit2, errors)
+  cnot0(bit1, bit2, errors)
+  hadamard0(bit2, errors)
+
+def dualcz0(bit1, bit2, errors): 
+  hadamard0(bit1, errors)
+  cnot0(bit1, bit2, errors)
+  hadamard0(bit1, errors)
+
+def prep0(bit, errors): 
+  errors.x ^= errors.x & (1<<bit)
+  errors.z ^= errors.z & (1<<bit)
+
+def prepZ(bit, errors, errorRates): 
+  prep0(bit, errors)
+  errorsX1(bit, errors, errorRates.prep)
+
+def prepX(bit, errors, errorRates): 
+  prep0(bit, errors)
+  errorsZ1(bit, errors, errorRates.prep)
+
+def measZ(bit, errors, errorRates): 
+  errorsX1(bit, errors, errorRates.meas)
+  return (errors.x>>bit)&1
+
+def measX(bit, errors, errorRates): 
+  errorsZ1(bit, errors, errorRates.meas)
+  return (errors.z>>bit)&1
+
+def cnot(bit1, bit2, errors, errorRates): 
+  cnot0(bit1, bit2, errors)
+  anisotropic_cx(bit1, bit2, errors, errorRates.cnot)
+  errors1(bit1, errors, errorRates.prep)
+  errors1(bit2, errors, errorRates.prep)
+
+def cz(bit1, bit2, errors, errorRates): 
+  cz0(bit1, bit2, errors)
+  anisotropic_cz(bit1, bit2, errors, errorRates.cnot)
+  errors1(bit1, errors, errorRates.prep)
+  errors1(bit2, errors, errorRates.prep)
+
+def anisotropic_cx(bit1, bit2, errors, prob):
+    if random.random() < prob:
+        # Apply X to bit1 (target), Z to bit2 (control)
+        
+        errors.z ^= 1 << bit1
+        errors.x ^= 1 << bit2
+        # errors2(bit1, bit2, errors, prob)
+
+def anisotropic_cz(bit1, bit2, errors, prob):
+    if random.random() < prob:
+        # Apply Z to both qubits
+        
+        errors.z ^= 1 << bit1
+        errors.z ^= 1 << bit2
+        # errors2(bit1, bit2, errors, prob)
+
+
+def anisotropic_cy(bit1, bit2, errors, prob):
+    if random.random() < prob:
+        # Apply Z to both qubits
+        errors.x ^= 1 << bit1
+        errors.z ^= 1 << bit1
+        errors.x ^= 1 << bit2
+        # errors2(bit1, bit2, errors, prob)
+
+def dualcz(bit1, bit2, errors, errorRates): 
+  dualcz0(bit1, bit2, errors)
+  errors2(bit1, bit2, errors, errorRates.cnot)
+
+def prep(bit, errors, errorRates): 
+  prep0(bit, errors)
+  errors1(bit, errors, errorRates.prep)
